4baaab2d38d955177e3baf1399fcce049e05ec01
ihollander <ianholla@gmail.com>
2021-08-04 06:02:58 -0400

    Fixed formatting

---
+++ get.rb
@@ -0,0 +1,10 @@
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json'
+
+uri = URI.parse(url)
+response = Net::HTTP.get_response(uri)
+
+p JSON.parse(response.body)
